eac57bc2e7b932d7476ce4c12155d36ce990a0cf
Ionut Capitanu <ionutcapitanu@Ionuts-MacBook-Air.local>
2021-02-21 15:04:00 +0200

    Added facebook messenger integration

--- frontend/src/components/FacebookMessengerChat.js
+++ frontend/src/components/FacebookMessengerChat.js
@@ -8,9 +8,10 @@ const FacebookMessengerChat = () => {
         pageId="512114519309523"
         appId="188699816368668"
         htmlRef={window.location.pathname}
-        shouldShowDialog="true"
         minimized="True"
-        loggedInGreeting="Discută cu Anca, consultantul tău Usborne, pentu a primi cele mai potrivite recomandări de cărți pentru copilul tău !"
+        loggedInGreeting="Discută cu Anca, consultantul tău Usborne !"
+        greetingDialogDisplay="show"
+        language="Romanian"
       />
     </div>
   );
